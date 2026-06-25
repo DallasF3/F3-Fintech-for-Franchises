676d8d5d5e35b7496c7aeae16d822e0f931c19c5
devwithhahsir <f2022065252@umt.edu.pk>
2026-06-25 23:29:32 +0500

    Allow self-signed certificates for DB connections in production

--- apps/backend/src/shared/database/knexfile.ts
+++ apps/backend/src/shared/database/knexfile.ts
@@ -59,14 +59,14 @@ const config: { [key: string]: Knex.Config } = {
     client: 'pg',
     connection: {
       ...(process.env.DATABASE_URL
-        ? { connectionString: process.env.DATABASE_URL, ssl: true }
+        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
         : {
             host: process.env.DB_HOST,
             port: parseInt(process.env.DB_PORT || '5432'),
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
-            ssl: true,
+            ssl: { rejectUnauthorized: false },
           }),
     },
     migrations: {
